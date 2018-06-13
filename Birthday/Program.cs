b1fbc06091d8975e505922f8c27a687cab574c25
Unknown <nkh.work1@gmail.com>
2018-06-13 13:04:45 -0500

    added birth

--- Birthday/Program.cs
+++ Birthday/Program.cs
@@ -7,6 +7,8 @@ namespace Birthday
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I want a walrus");
+            Console.ReadKey();
         }
     }
 }
