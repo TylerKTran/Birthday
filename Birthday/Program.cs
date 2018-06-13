8f34a5c45f983c319586a0f58709d24cc141e560
Unknown <nkh.work1@gmail.com>
2018-06-13 13:36:15 -0500

    mariam tries to live

--- Birthday/Program.cs
+++ Birthday/Program.cs
@@ -12,6 +12,7 @@ namespace Birthday
             Console.WriteLine("I want a walrus");
             Console.WriteLine("50 Thousand on my head that's disrespect.");
             Console.WriteLine("What I want for my Birthday:\nA Ferrari");
+            Console.WriteLine("Mariam !");
             Console.ReadKey();
         }
     }
