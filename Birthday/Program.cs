4686e7cac35b08b9c3c616d424c1541f45ff8460
mridulwaldia <mridulsinghwaldia@gmail.com>
2018-06-13 13:10:05 -0500

    Added my request and console window open

--- Birthday/Program.cs
+++ Birthday/Program.cs
@@ -7,6 +7,8 @@ namespace Birthday
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("50 Thousand on my head that's disrespect.");
+            Console.ReadKey();
         }
     }
 }
